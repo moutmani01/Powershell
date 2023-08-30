1ca1f2f38ec0a220a289f2678f51c0f3eefcdd0f
moutmani01 <57376576+moutmani01@users.noreply.github.com>
2023-08-30 11:15:21 +0100

    Update signoff_disconnected_user.ps1

--- signoff_disconnected_user.ps1
+++ signoff_disconnected_user.ps1
@@ -1,16 +1,27 @@
-I apologize for the inconvenience. It seems like there might be compatibility issues or variations in the `query user` command output based on the Windows version or locale.
-
-As an alternative, you could consider using PowerShell, which provides more reliable ways to handle this task across different Windows environments. Here's a PowerShell script that should log off all disconnected users and display messages for each logoff:
+I'm glad to hear that the script worked for you! Here's the modified PowerShell script that will log off users who have been disconnected for more than 1 day:
 
 ```powershell
 $queryResult = quser
-$disconnectedUsers = $queryResult | Where-Object { $_ -match "Disc" }
+$disconnectedUsers = $queryResult | Select-String "Disc"
+
+$currentDate = Get-Date
 
 foreach ($userLine in $disconnectedUsers) {
-    $sessionId = $userLine -replace '\s+', ' ' -split ' ')[2]
-    logoff $sessionId
-    Write-Host "User $sessionId has been logged off."
+    $sessionId = ($userLine -split '\s+')[2]
+    $idleTime = ($userLine -split '\s+')[4]
+
+    $idleHours = [int]($idleTime -replace "(\d+)\:(\d+)", '$1')
+    $idleMinutes = [int]($idleTime -replace "(\d+)\:(\d+)", '$2')
+
+    $totalIdleTime = ($idleHours * 60) + $idleMinutes
+
+    if ($totalIdleTime -gt 1440) {  # 1440 minutes = 1 day
+        logoff $sessionId
+        Write-Host "User $sessionId has been logged off."
+    }
 }
 ```
 
-To use this script, open PowerShell and run it. It should log off disconnected users and display messages accordingly. Remember to test it in a controlled environment before using it in a production setting.
+This script calculates the total idle time in minutes, and if it's greater than 1440 minutes (which corresponds to 1 day), it logs off the user and displays a message.
+
+As always, make sure to test the script in a controlled environment before using it in a production setting.
