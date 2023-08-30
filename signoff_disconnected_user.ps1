3115710a95500470efa0ef2776a583718a71062b
moutmani01 <57376576+moutmani01@users.noreply.github.com>
2023-08-30 10:44:31 +0100

    Update signoff_disconnected_user.ps1

--- signoff_disconnected_user.ps1
+++ signoff_disconnected_user.ps1
@@ -1,1 +1,13 @@
-356569
+Sure, here's a PowerShell script that will help you delete sessions that have been disconnected for more than 3 days:
+
+```powershell
+# Get the current date
+$currentDate = Get-Date
+
+# Get disconnected sessions older than 3 days
+$disconnectedSessions = Get-RDUserSession | Where-Object { $_.State -eq 'Disconnected' -and ($currentDate - $_.DisconnectTime).Days -gt 3 }
+
+# Delete the sessions
+foreach ($session in $disconnectedSessions) {
+    Invoke-RDUserLogoff -UnifiedSessionID $session.UnifiedSessionID -Force
+}
